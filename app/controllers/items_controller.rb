762f2a9d874a672099bd202603d723d98d87ba89
Matthew Phillips <phillips.matthew829@gmail.com>
2022-06-27 15:05:10 -0500

    commit tree 5bb8d25cf891bbec1469c4504d980d5d838e4d03

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,13 @@
 class ItemsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        items = Item.all.order(user_id: :desc)
+        render json: items, include: :user, status: :ok
+    end
+
+    private
+    def render_not_found_response
+        render json: {error: "Item not found"}, status: :not_found
+    end
 end
